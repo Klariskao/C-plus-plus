2e6f8bf93e014299f1ef09cf38f06d2c028d707a
Klariskao <64138765+Klariskao@users.noreply.github.com>
2021-07-15 15:33:55 +0100

    Add files via upload

---
+++ 8_string_from_backwards/8_string_from_backwards+/8_string_from_backwards+.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+std::string original = "Hellooo";
+
+void flipping(std::string x)
+{
+    for (int i = size(x) - 1; i >= 0; i--)
+    {
+        std::cout << x[i];
+    }
+}
+
+int main()
+{
+    flipping(original);
+}
